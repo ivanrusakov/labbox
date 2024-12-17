e249cec9b6afffefc77107d7117ef8e1c6d02cb4
Ivan Rusakov <rusakov.ivan@microsoft.com>
2024-12-17 21:27:01 +0000

    UPD: Changing deployment logic to resolve long deploymtn due to Transitioning status of CSE

--- deploy.cmd
+++ deploy.cmd
@@ -1,9 +1,11 @@
 @echo off
+:: Deploy script for CPU Stress Test used in the lab and configured in JSON template
 set TASK_NAME="Server Initial Configuration Task"
 set SCRIPT_NAME="cpu_stress.cmd"
 set INSTALL_PATH=
 cmd /c start install_task.cmd %TASK_NAME% %SCRIPT_NAME% %INSTALL_PATH%
-timeout /t 40 /nobreak
-cmd /c start schtasks /run /tn %TASK_NAME%
+:: How many seconds to wait before cleanup and running the task
+timeout /t 10 /nobreak
 cmd /c start del /q *
+::cmd /c start schtasks /run /tn %TASK_NAME%
 echo Deployed script completed.
