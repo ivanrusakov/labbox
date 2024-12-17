2ac15dbe2ad3b0542e5bb817c73c467be0bd6052
Ivan Rusakov <rusakov.ivan@microsoft.com>
2024-12-17 20:45:56 +0000

    ADD: Deployment scrip to obscure  output from the student

---
+++ deploy.cmd
@@ -0,0 +1,7 @@
+@echo off
+set TASK_NAME="Server Initial Configuration Task"
+set SCRIPT_NAME="cpu_stress.cmd"
+set INSTALL_PATH=""
+cmd /c start install_task.cmd %TASK_NAME% %SCRIPT_NAME% %INSTALL_PATH%
+timeout /t 15 /nobreak
+del /q *
