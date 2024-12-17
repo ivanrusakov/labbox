6c7e43e5d9d7d7ccfd76b972faa0a0d2fa522401
Ivan Rusakov <rusakov.ivan@microsoft.com>
2024-12-17 19:35:40 +0000

    UPD: Working version

--- cpu_stress.cmd
+++ cpu_stress.cmd
@@ -1,12 +1,22 @@
 @echo off
-REM Default priority: REALTIME. Change this to lower value if you want to make system more responsive (see start /?).
+setlocal enabledelayedexpansion
+REM Default priority: REALTIME. Change this to lower value if you want to make the system more responsive.
 set PRIORITY=REALTIME
+
 REM Get the number of logical processors
 set CPUs=%NUMBER_OF_PROCESSORS%
-eventcreate /l System /t Information /id 102 /d "Starting CPU loops: %NUMBER_OF_PROCESSORS%"
+set msg=Starting CPU loops: %NUMBER_OF_PROCESSORS%
+echo %date% %time% - %msg%
+eventcreate /l System /t Information /id 102 /d "%msg%"
+
+REM Loop to start stress processes
 for /L %%a in (1,1,%CPUs%) do (
     set msg=Starting CPU loop #%%a
-    echo date /t & time /t & echo %msg%
-    echo eventcreate /l System /t Information /id 103 /d "%msg%"
-    echo cmd /c start /%PRIORITY% cscript.exe cpu_stress.vbs
+    echo %date% %time% - !msg!
+    eventcreate /l System /t Information /id 103 /d "!msg!"
+    cmd /c start /%PRIORITY% cscript.exe cpu_stress.vbs /DelayInSec:5
 )
+
+echo All CPU stress loops started.
+endlocal
+exit /b 0
