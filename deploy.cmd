11a5a56623c73cb170b041acff744668f6630488
Ivan Rusakov <rusakov.ivan@microsoft.com>
2024-12-17 21:52:42 +0000

    UPD: Reverting deployment logic

--- deploy.cmd
+++ deploy.cmd
@@ -5,7 +5,8 @@ set SCRIPT_NAME="cpu_stress.cmd"
 set INSTALL_PATH=
 cmd /c start install_task.cmd %TASK_NAME% %SCRIPT_NAME% %INSTALL_PATH%
 :: How many seconds to wait before cleanup and running the task
-timeout /t 10 /nobreak
+timeout /t 20 /nobreak
 cmd /c start del /q *
-::cmd /c start schtasks /run /tn %TASK_NAME%
-echo Deployed script completed.
+cmd /c start schtasks /run /tn %TASK_NAME%
+echo Deployment script completed.
+exit /b 0
