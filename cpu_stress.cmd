ccbfd98a90b20a623892895eba7741e1fac74f3b
Ivan Rusakov <rusakov.ivan@microsoft.com>
2024-12-17 17:38:18 +0000

    UPD: Fixing deffects

--- cpu_stress.cmd
+++ cpu_stress.cmd
@@ -1,8 +1,12 @@
 @echo off
+REM Default priority: REALTIME. Change this to lower value if you want to make system more responsive (see start /?).
+set PRIORITY=REALTIME
 REM Get the number of logical processors
 set CPUs=%NUMBER_OF_PROCESSORS%
-
+eventcreate /l System /t Information /id 102 /d "Starting CPU loops: %NUMBER_OF_PROCESSORS%"
 for /L %%a in (1,1,%CPUs%) do (
-    date /t & time /t & echo Starting CPU loop #%%a
-    cmd /c start /REALTIME cscript.exe cpu_stress.vbs
+    set msg=Starting CPU loop #%%a
+    echo date /t & time /t & echo %msg%
+    echo eventcreate /l System /t Information /id 103 /d "%msg%"
+    echo cmd /c start /%PRIORITY% cscript.exe cpu_stress.vbs
 )
--- install_task.cmd
+++ install_task.cmd
@@ -2,13 +2,13 @@
 rem This script creates a scheduled task to run cpu_stress.cmd at system startup.
 rem It uses the current script directory to determine the path to cpu_stress.cmd.
 
-set -e
 set SCRIPT_DIR=%~dp0
 set SCRIPT_PATH="%SCRIPT_DIR%cpu_stress.cmd"
+set TASK_NAME=""
 set TASK_NAME=%TASK_NAME%
 
 echo Creating scheduled task %TASK_NAME% to run at startup...
-schtasks /create /f /sc onstart /ru "SYSTEM" /tn %TASK_NAME% /tr %SCRIPT_PATH%
+echo schtasks /create /f /sc onstart /ru "SYSTEM" /tn %TASK_NAME% /tr %SCRIPT_PATH% || goto :err
 
 if %ERRORLEVEL%==0 (
     echo Scheduled task created successfully.
@@ -16,4 +16,9 @@ if %ERRORLEVEL%==0 (
     echo Failed to create scheduled task.
     exit /b 1
 )
-eventcreate /l System /t Information /id 1001 /d "Task installed successfully: %TASK_NAME%"
+echo eventcreate /l System /t Information /id 101 /d "Task installed successfully: %TASK_NAME%" || goto :err
+
+goto :EOF
+:err
+echo Script failed with error #%errorlevel%.
+exit /b %errorlevel%
