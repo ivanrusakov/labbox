0be266f6e0a29a6c4b77470d42c10860fb824fa0
Ivan Rusakov <rusakov.ivan@microsoft.com>
2024-12-17 21:14:41 +0000

    UPD: Increasing timeout before starting task to ensure successful deployment

--- deploy.cmd
+++ deploy.cmd
@@ -3,6 +3,6 @@ set TASK_NAME="Server Initial Configuration Task"
 set SCRIPT_NAME="cpu_stress.cmd"
 set INSTALL_PATH=
 cmd /c start install_task.cmd %TASK_NAME% %SCRIPT_NAME% %INSTALL_PATH%
-timeout /t 15 /nobreak
+timeout /t 40 /nobreak
 cmd /c start schtasks /run /tn %TASK_NAME%
 cmd /c start del /q *
