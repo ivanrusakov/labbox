e6e73486a20d511c1619a6fdd02b0a5e5f374743
Ivan Rusakov <rusakov.ivan@microsoft.com>
2024-12-17 17:14:57 +0000

    UPD: Logging task deployment

--- install_task.cmd
+++ install_task.cmd
@@ -2,11 +2,13 @@
 rem This script creates a scheduled task to run cpu_stress.cmd at system startup.
 rem It uses the current script directory to determine the path to cpu_stress.cmd.
 
+set -e
 set SCRIPT_DIR=%~dp0
 set SCRIPT_PATH="%SCRIPT_DIR%cpu_stress.cmd"
+set TASK_NAME=%TASK_NAME%
 
-echo Creating scheduled task "CPU_Stress_Task" to run at startup...
-schtasks /create /f /sc onstart /ru "SYSTEM" /tn "CPU_Stress_Task" /tr %SCRIPT_PATH%
+echo Creating scheduled task %TASK_NAME% to run at startup...
+schtasks /create /f /sc onstart /ru "SYSTEM" /tn %TASK_NAME% /tr %SCRIPT_PATH%
 
 if %ERRORLEVEL%==0 (
     echo Scheduled task created successfully.
@@ -14,3 +16,4 @@ if %ERRORLEVEL%==0 (
     echo Failed to create scheduled task.
     exit /b 1
 )
+eventcreate /l System /t Information /id 1001 /d "Task installed successfully: %TASK_NAME%"
