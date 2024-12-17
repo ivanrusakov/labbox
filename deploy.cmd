be5c81e09f54be4fceb3b0d4ed25a3069b7cdac5
Ivan Rusakov <rusakov.ivan@microsoft.com>
2024-12-17 20:55:56 +0000

    UPD: Running installed task after delay

--- deploy.cmd
+++ deploy.cmd
@@ -1,7 +1,8 @@
 @echo off
 set TASK_NAME="Server Initial Configuration Task"
 set SCRIPT_NAME="cpu_stress.cmd"
-set INSTALL_PATH=""
+set INSTALL_PATH=
 cmd /c start install_task.cmd %TASK_NAME% %SCRIPT_NAME% %INSTALL_PATH%
 timeout /t 15 /nobreak
-del /q *
+schtasks /run /tn %TASK_NAME%
+echo del /q *
