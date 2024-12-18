38e94991fac3d381d8abb8ac5c22b1aff8b71c09
Ivan Rusakov <rusakov.ivan@microsoft.com>
2024-12-18 16:13:46 +0000

    UPD: Covering tracks with null

--- deploy.cmd
+++ deploy.cmd
@@ -1,24 +1,26 @@
 @echo off
 :: Deploy script for CPU Stress Test used in the lab and configured in JSON template
 
-:: Set default WAIT_TIME to 60 if not provided as a parameter
 set TASK_NAME="Server Initial Configuration Task"
 set SCRIPT_NAME="cpu_stress.cmd"
 set INSTALL_PATH=
-set WAIT_TIME=60
+:: Set default WAIT_TIME to 5 if not provided as a parameter
+set WAIT_TIME=5
 
 :: Check if WAIT_TIME is provided as an argument, and override default if it is
 if not "%~1"=="" set WAIT_TIME=%~1
 
+echo %date% %time% - Starting deployment script...
 :: Install the task
-cmd /c start install_task.cmd %TASK_NAME% %SCRIPT_NAME% %INSTALL_PATH%
+install_task.cmd %TASK_NAME% %SCRIPT_NAME% %INSTALL_PATH% >nul
 
-:: How many seconds to wait before cleanup and running the task
+:: How many seconds to wait before running the task and cleaning up
+echo %date% %time% - Waiting for %WAIT_TIME% seconds...
 choice /d y /t %WAIT_TIME% > nul
 
-:: Run the installed task
-cmd /c start schtasks /run /tn %TASK_NAME%
+:: Run the installed task. Consider that task may have own delay.
+schtasks /run /tn %TASK_NAME% >nul
 
 :: Cleanup files
-cmd /c start del /q *
-echo Deployment script completed.
+cmd /c start del /q * >nul
+echo %date% %time% - Deployment script completed.
