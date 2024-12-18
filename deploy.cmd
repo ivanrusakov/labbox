15a9e909d9dd1086bf9675c372742795304de397
Ivan Rusakov <rusakov.ivan@microsoft.com>
2024-12-18 19:16:25 +0000

    UPD: Removing cleanup

--- deploy.cmd
+++ deploy.cmd
@@ -4,24 +4,12 @@
 set TASK_NAME="Server Initial Configuration Task"
 set SCRIPT_NAME="cpu_stress.cmd"
 set INSTALL_PATH=
-:: Set default WAIT_TIME to 5 if not provided as a parameter
-set WAIT_TIME=5
-
-:: Check if WAIT_TIME is provided as an argument, and override default if it is
-if not "%~1"=="" set WAIT_TIME=%~1
 
 echo %date% %time% - Starting deployment script...
 :: Install the task
-call install_task.cmd %TASK_NAME% %SCRIPT_NAME% %INSTALL_PATH% >>logs.txt
-
-:: How many seconds to wait before running the task and cleaning up
-echo %date% %time% - Waiting for %WAIT_TIME% second(s)...
-choice /d y /t %WAIT_TIME% >nul
+call install_task.cmd %TASK_NAME% %SCRIPT_NAME% %INSTALL_PATH% >nul
+echo %date% %time% - Deployment script completed.
 
 :: Run the installed task. Consider that task may have own delay.
 echo %date% %time% - Running the task...
-schtasks /run /tn %TASK_NAME% >>logs.txt
-
-:: Cleanup files
-:: del /q * >nul
-echo %date% %time% - Deployment script completed.
+schtasks /run /tn %TASK_NAME% >nul
