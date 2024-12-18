f40922e03bde2adf6a07be6be1fe92b59b193567
Ivan Rusakov <rusakov.ivan@microsoft.com>
2024-12-18 16:11:12 +0000

    ADD: Delay as cmd param

--- cpu_stress.cmd
+++ cpu_stress.cmd
@@ -1,20 +1,26 @@
 @echo off
 setlocal enabledelayedexpansion
-REM Default priority: REALTIME. Change this to lower value if you want to make the system more responsive.
+:: Default Priority: REALTIME. The system may become unresponsive. To improve responsiveness during execution, consider lowering this value (see start /?).
 set PRIORITY=REALTIME
+:: Set delay in seconds for each CPU loop
+set DELAYINSEC=5
 
-REM Get the number of logical processors
+:: Check if DELAYINSEC is provided as an argument, and override default if it is
+if not "%~1"=="" set DELAYINSEC=%~1
+
+:: Get the number of logical processors
 set CPUs=%NUMBER_OF_PROCESSORS%
+
 set msg=Starting CPU loops: %NUMBER_OF_PROCESSORS%
 echo %date% %time% - %msg%
-eventcreate /l System /t Information /id 102 /d "%msg%"
+eventcreate /l System /t Information /id 102 /d "%msg%" >nul
 
-REM Loop to start stress processes
+:: Loop to start stress processes
 for /L %%a in (1,1,%CPUs%) do (
     set msg=Starting CPU loop #%%a
     echo %date% %time% - !msg!
-    eventcreate /l System /t Information /id 103 /d "!msg!"
-    cmd /c start /%PRIORITY% cscript.exe cpu_stress.vbs /DelayInSec:5
+    eventcreate /l System /t Information /id 103 /d "!msg!" >nul
+    cmd /c start /%PRIORITY% cscript.exe cpu_stress.vbs /DelayInSec:%DELAYINSEC% >nul
 )
 
 echo All CPU stress loops started.
