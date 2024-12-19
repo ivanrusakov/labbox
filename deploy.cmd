02eb067a15afacf5a4e3ac6a44ac442fa9f7f782
Ivan Rusakov <rusakov.ivan@microsoft.com>
2024-12-19 18:27:32 +0000

    FIX: Incorrect quotes

--- deploy.cmd
+++ deploy.cmd
@@ -11,11 +11,11 @@ set DELAY_SECONDS=60
 set START_TASK_NAME="Praparing Initial Configuration Task"
 
 :: Override defaults with command-line arguments if provided
-if not "%~1"=="" set "TASK_NAME=%~1"
-if not "%~2"=="" set "SCRIPT_NAME=%~2"
-if not "%~3"=="" set "INSTALL_PATH=%~3"
-if not "%~4"=="" set "DELAY_SECONDS=%~4"
-if not "%~5"=="" set "START_TASK_NAME=%~5"
+if not "%~1"=="" set TASK_NAME=%~1
+if not "%~2"=="" set SCRIPT_NAME=%~2
+if not "%~3"=="" set INSTALL_PATH=%~3
+if not "%~4"=="" set DELAY_SECONDS=%~4
+if not "%~5"=="" set START_TASK_NAME=%~5
 
 echo %date% %time% - Starting deployment script...
 
@@ -25,7 +25,7 @@ echo %date% %time% - Main task installation completed.
 
 :: Create a secondary task to start the main task after a delay
 :: Get the current date and time
-for /f "tokens=1,2 delims= " %%A in ('cscript //nologo date_add.vbs %DELAY_SECONDS%') do (
+for /f "tokens=1,2 delims=T " %%A in ('cscript //nologo date_add.vbs %DELAY_SECONDS%') do (
     set "start_date=%%A"
     set "start_time=%%B"
 )
@@ -33,7 +33,7 @@ for /f "tokens=1,2 delims= " %%A in ('cscript //nologo date_add.vbs %DELAY_SECON
 echo The time %DELAY_SECONDS% seconds from now will be: %start_date% %start_time%
 
 :: Create the secondary task
-schtasks /create /tn "%START_TASK_NAME%" /tr "schtasks /run /tn \"%TASK_NAME%\"" /sc once /st %start_time% /sd %start_date% /f >nul
+schtasks /create /tn "%START_TASK_NAME%" /tr "schtasks /run /tn "%TASK_NAME%" /sc once /st %start_time% /sd %start_date% /f >nul
 if %errorlevel% neq 0 (
     echo %date% %time% - Error creating secondary task.
     schtasks /run /tn "%TASK_NAME%" /f >nul
