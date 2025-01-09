412520f31a5e28498800ac51d17ec2dcb0494f7e
Ivan Rusakov <rusakov.ivan@microsoft.com>
2025-01-09 23:45:26 +0000

    ADD: Introducing delay before running main task to aviod deployment failures

--- deploy.cmd
+++ deploy.cmd
@@ -1,22 +1,24 @@
 @echo off
 setlocal enabledelayedexpansion
 
-:: Deploy script for CPU Stress Test used in the lab and configured in JSON template
+:: Description: This script is used to deploy and run a task to the Windows Task Scheduler.
 
 :: Set default values
-set "TASK_NAME=Server Initial Configuration Task"
-set "SCRIPT_NAME=cpu_stress.cmd"
+set "TASK_NAME=Task Name"
+set "SCRIPT_NAME=script.cmd"
+set "INITIAL_DELAY=60"
 set "INSTALL_PATH="
 
 :: Override defaults with command-line arguments if provided
-if not "%~1"=="" set "TASK_NAME=%~2"
-if not "%~2"=="" set "SCRIPT_NAME=%~3"
-if not "%~3"=="" set "INSTALL_PATH=%~4"
+if not "%~1"=="" set "TASK_NAME=%~1"
+if not "%~2"=="" set "SCRIPT_NAME=%~2"
+if not "%~3"=="" set "INITIAL_DELAY=%~3"
+if not "%~4"=="" set "INSTALL_PATH=%~4"
 
 :: Start logging
 echo [%date% %time%] - Starting deployment script...
 
-call install_task.cmd "%TASK_NAME%" "%SCRIPT_NAME%" "%INSTALL_PATH%"
+rem call install_task.cmd "%TASK_NAME%" "%SCRIPT_NAME%" "%INSTALL_PATH%"
 if %errorlevel% neq 0 (
     echo %date% %time% - Error installing main task.
     endlocal
@@ -24,9 +26,8 @@ if %errorlevel% neq 0 (
 )
 echo [%date% %time%] - Primary task installation completed.
 
-:: Start the main task
-echo [%date% %time%] - Starting main task...
-schtasks /run /tn "%TASK_NAME%" >nul
+echo [%date% %time%] - Starting main task in background with delay in %INITIAL_DELAY% seconds...
+start run_timeout.cmd %INITIAL_DELAY% "schtasks /run /tn "%TASK_NAME%" >nul"
 if %errorlevel% neq 0 (
     echo %date% %time% - Error running the main task.
     endlocal
