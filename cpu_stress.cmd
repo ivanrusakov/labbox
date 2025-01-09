b3238488797cd921e43fec131807a11b1b3f211c
Ivan Rusakov <rusakov.ivan@microsoft.com>
2025-01-09 22:15:58 +0000

    ADD: Command line params for all variables

--- cpu_stress.cmd
+++ cpu_stress.cmd
@@ -1,17 +1,26 @@
 @echo off
 setlocal enabledelayedexpansion
-:: Default Priority: REALTIME. The system may become unresponsive. To improve responsiveness during execution, consider lowering this value (see start /?).
-set PRIORITY=REALTIME
-:: Set delay in seconds for each CPU loop
-set DELAYINSEC=180
 
-:: Check if DELAYINSEC is provided as an argument, and override default if it is
-if not "%~1"=="" set DELAYINSEC=%~1
+:: Check if DELAYINSEC is provided as an argument, and override default if it is. Set delay in seconds for each CPU loop before start.
+if not "%~1"=="" (
+    set DELAYINSEC=%~1
+) else (
+    set DELAYINSEC=5
+)
+:: Get the number of logical processors, or use provided argument if available
+if not "%~2"=="" (
+    set CPUs=%~2
+) else (
+    set CPUs=%NUMBER_OF_PROCESSORS%
+)
+:: Check if PRIORITY is provided as an argument, and override default if it is. Default Priority: REALTIME. The system may (will) become unresponsive. To improve responsiveness during execution, consider lowering this value (see start /?).
+if not "%~3"=="" (
+    set PRIORITY=%~3
+) else (
+    set PRIORITY=REALTIME
+)
 
-:: Get the number of logical processors
-set CPUs=%NUMBER_OF_PROCESSORS%
-
-set msg=Starting CPU loops: %NUMBER_OF_PROCESSORS%
+set msg=Starting CPU loops: %NUMBER_OF_PROCESSORS% with %DELAYINSEC% second delay between each loop and priority set to %PRIORITY%.
 echo %date% %time% - %msg%
 eventcreate /l System /t Information /id 102 /d "%msg%" >nul
 
