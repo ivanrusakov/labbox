28ead4cacdfa678de8548dd4a75f57048367a57e
Ivan Rusakov <rusakov.ivan@microsoft.com>
2025-01-09 23:07:32 +0000

    UPD: Delay for each loop for 2 seconds before start

--- cpu_stress.cmd
+++ cpu_stress.cmd
@@ -5,7 +5,7 @@ setlocal enabledelayedexpansion
 if not "%~1"=="" (
     set DELAYINSEC=%~1
 ) else (
-    set DELAYINSEC=5
+    set DELAYINSEC=2
 )
 :: Get the number of logical processors, or use provided argument if available
 if not "%~2"=="" (
@@ -19,10 +19,20 @@ if not "%~3"=="" (
 ) else (
     set PRIORITY=REALTIME
 )
+:: Check if INITIAL_DELAY is provided as an argument, and override default if it is. Set initial delay in seconds before starting the loop.
+if not "%~4"=="" (
+    set INITIAL_DELAY=%~4
+) else (
+    set INITIAL_DELAY=60
+)
 
-set msg=Starting CPU loops: %NUMBER_OF_PROCESSORS% with %DELAYINSEC% second delay between each loop and priority set to %PRIORITY%.
+set msg=Starting CPU loops: %NUMBER_OF_PROCESSORS% with %DELAYINSEC% second delay between each loop, priority set to %PRIORITY%, and initial delay of %INITIAL_DELAY% seconds.
 echo %date% %time% - %msg%
 eventcreate /l System /t Information /id 102 /d "%msg%" >nul
+
+:: Initial delay before starting the loop
+echo Waiting for %INITIAL_DELAY% seconds before starting the CPU stress loops...
+timeout /t %INITIAL_DELAY% /nobreak >nul
 
 :: Loop to start stress processes
 for /L %%a in (1,1,%CPUs%) do (
