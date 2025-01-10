97abf744a3c42b854bb041f55281690d926ea3f6
Ivan Rusakov <rusakov.ivan@microsoft.com>
2025-01-10 00:06:24 +0000

    FIX: Incorrect behaviour when it deleting itself

--- cleanup.cmd
+++ cleanup.cmd
@@ -4,13 +4,13 @@ set WAIT_TIME=5
 :: Check if WAIT_TIME is provided as an argument, and override default if it is
 if not "%~1"=="" set WAIT_TIME=%~1
 
-:: Get the script's full path
-set SCRIPT_PATH="%~f0"
+:: Get the script's name
+set SCRIPT_NAME="%~nx0"
 
 :: Cleanup files
 echo %date% %time% - Running cleanup...
 for %%f in (*) do (
-    if not "%%~f"==%SCRIPT_PATH% del /q "%%f" >nul
+    if not "%%~f"==%SCRIPT_NAME% del /q "%%f"
 )
 
 echo %date% %time% - Cleanup script completed.
